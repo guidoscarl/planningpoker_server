12237eeca08b72b39c66c4221bb5258dfc154ff1
guido.scarlato@nttdata.com <guido.scarlato@nttdata.com>
2022-10-27 14:18:17 +0200

    server updates

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { all } = require('express/lib/application');
 
 var app = express()
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', "https://planningpoker-c.herokuapp.com/);
+    res.setHeader('Access-Control-Allow-Origin', "https://planningpoker-c.herokuapp.com/");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();
