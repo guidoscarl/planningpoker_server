b6bc42f7ae26ee672164a47819d7197e54fcebc8
guido.scarlato@nttdata.com <guido.scarlato@nttdata.com>
2022-11-17 15:00:48 +0100

    changed cors policy

--- index.js
+++ index.js
@@ -2,33 +2,33 @@ const express = require('express');
 const socket = require('socket.io');
 const colors = require('colors');
 const cors = require('cors');
-const {createRoom, joinRoom, sendVote, resetRoom, handleSocketDisconnection, getSocketRoom} = require("./models/Rooms");
+const { createRoom, joinRoom, sendVote, resetRoom, handleSocketDisconnection, getSocketRoom } = require("./models/Rooms");
 const { all } = require('express/lib/application');
 
 var app = express()
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', "https://planningpoker-c.herokuapp.com/");
+    res.setHeader('Access-Control-Allow-Origin', ["https://planningpoker-c.herokuapp.com/", "http://planningpoker-c.herokuapp.com/"]);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     next();
-    }
+}
 );
-var server = app.listen(process.env.PORT || 5000, ()=>{console.log("server started")});
+var server = app.listen(process.env.PORT || 5000, () => { console.log("server started") });
 
 const io = socket(server, {
     cors: {
-        origin: "https://planningpoker-c.herokuapp.com",
+        origin: ["https://planningpoker-c.herokuapp.com", "http://planningpoker-c.herokuapp.com/"],
         methods: ["GET", "POST"],
-        credentials:true
-      }
+        credentials: true
+    }
 });
-io.on("connection", (socket)=>{
+io.on("connection", (socket) => {
 
     socket.on("createRoom", (username, votingSystem) => {
         var roomId = createRoom(username, votingSystem, socket.id);
         console.log(votingSystem)
         socket.join(roomId)
-        socket.emit('roomCreated', roomId)       
+        socket.emit('roomCreated', roomId)
     }),
 
     socket.on("joinRoom", (data) => {
@@ -36,10 +36,10 @@ io.on("connection", (socket)=>{
         if (result === "RoomNotExist") {
             io.to(socket.id).emit("RoomNotExist")
         }
-        else if (result === "RoomFull"){
+        else if (result === "RoomFull") {
             io.to(socket.id).emit("RoomFull")
         }
-        else if (result === "UserNameAlreadyExist"){
+        else if (result === "UserNameAlreadyExist") {
             io.to(socket.id).emit("UserNameAlreadyExist")
         }
         else {
@@ -47,7 +47,7 @@ io.on("connection", (socket)=>{
             io.to(data.roomId).emit('userJoined', (result))
         }
     });
-   
+
     socket.on("sendVote", (data) => {
         var allUsers = sendVote(data.userName, data.roomId, data.vote)
         io.to(data.roomId).emit("sendedVote", allUsers)
@@ -62,7 +62,7 @@ io.on("connection", (socket)=>{
         io.to(roomId).emit("newVoteRequested", allUsers)
     })
 
-    socket.on("disconnect", (data) => {       
+    socket.on("disconnect", (data) => {
         roomId = getSocketRoom(socket.id)
         allUsers = handleSocketDisconnection(socket.id)
         io.to(roomId).emit("userDisconnected", allUsers)
